6f3aa394e680832b92652c342ac284fda20197e0
scottbishop <sa_bishop@comcast.net>
2021-06-10 15:33:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+for (let i = 1; i < 11; i++){
+    console.log(i)
+}
+
+for (let i = 10; i > 0; i--){
+    console.log(i)
+}
+
+let i = 1;
+
+while (i < 11) {
+    console.log(i);
+    i++;
+}
+
+let o = 10;
+
+while (o > 0) {
+    console.log(o);
+    o--;
+}
+
+let x = 1
+    do {
+        console.log(x);
+        x++
+    }
+
+    while (x < 11)
+
+let y = 10
+    do {
+        console.log(y);
+        y--
+    }
+
+    while (y > 0)
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+    for (let q of numbers) {
+        console.log (q)
+    }
